f5013f9a0c14c4b8662a824e60142f7b713e8112
zertu <290055513@qq.com>
2017-02-14 17:26:41 +0800

    调教vscode允许使用装饰器

--- src/app/app.js
+++ src/app/app.js
@@ -1,17 +1,15 @@
-import React,{Component} from 'react';
-import cssModule from 'react-css-modules';
-import styles from './app.css';
-import {observer} from 'mobx-react';
+import React,{Component} from 'react'
+import cssModule from 'react-css-modules'
+import styles from './app.css'
+import {observer} from 'mobx-react'
 
 @observer class TodoView extends React.Component {
     render() {
         return <div>{this.props.todo.title}</div>
     }
 }
-const TodoView = observer(({todo}) => <div>{todo.title}</div>)
-
 export default TodoView
-export const TodoView
+export const TodoViews = observer(({todo}) => <div>{todo.title}</div>)
 /*
 const app = () => (
   <div>
