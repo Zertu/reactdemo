6d375a3ee85f6946248b770cce28aae202233e9b
zertu <290055513@qq.com>
2017-02-19 22:42:27 +0800

    react 动画 貌似官方文档有问题没有添加class

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import store from './todolist/TodoStore'
 import TodoList from './todolist/TodoList'
+import ReactAnimation from './reactanimation/animation'
 const render = (Component) => {
   ReactDOM.render(
       Component,
@@ -10,7 +11,7 @@ const render = (Component) => {
   )
 }
 
-render(<TodoList store={store}/>)
+render(<ReactAnimation />)
 // const element = <Welcome name="Sara" />
 // ReactDOM.render(
 //   element,
