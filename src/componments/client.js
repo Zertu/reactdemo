771b1a144a308066e6226b0db38c062f2060fe57
zertu <290055513@qq.com>
2017-02-22 20:39:07 +0800

    lesson4

--- src/componments/client.js
+++ src/componments/client.js
@@ -1,16 +1,5 @@
 import React, { Component } from 'react'
 
-import {createStore} from 'redux'
-
-const reducer=(state,action)=>{
-    console.log(action)
-    return state++
-}
-const store = createStore(reducer,0)
-store.subscribe(()=>{
-    console.log(store.getState())
-})
-store.dispatch({type:'fuck',payload:1})
 class Layout extends Component {
     render() {
         return (
--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
+import {combineReducers, createStore} from 'redux'
+
 import Calculator from './app/app.js'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import store from './todolist/TodoStore'
-import TodoList from './todolist/TodoList'
 import ReactAnimation from './reactanimation/animation'
+import ReactDOM from 'react-dom'
+import TodoList from './todolist/TodoList'
+
+const userReducer = (state = {}, action) => {
+  return state
+}
+const tweetReducer = (state = [], action) => {
+  return state
+}
+const reducers = combineReducers({user: userReducer, tweets: tweetReducer})
 const render = (Component) => {
-  ReactDOM.render(
-      Component,
-    document.getElementById('root')
-  )
+  ReactDOM.render(Component, document.getElementById('root'))
 }
 
-render(<ReactAnimation />)
-// const element = <Welcome name="Sara" />
-// ReactDOM.render(
-//   element,
-//   document.getElementById('root')
-// )
+render(<ReactAnimation/>)
+
+const reducer = (state, action) => {
+  console.log(action.payload)
+  return state
+}
+const store = createStore(reducer, {
+  user: {
+    name: 'fuck',
+    age: 12
+  },
+  tweets: []
+})
+store.subscribe(() => {
+  console.log(store.getState())
+})
+store.dispatch({type: 'CHANGE_AGE', payload: 222222})
+// const element = <Welcome name="Sara" /> ReactDOM.render(   element,
+// document.getElementById('root') )
