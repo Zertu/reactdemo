2fe7ff590050701fd1025258ec295914d2b214f6
zertu <290055513@qq.com>
2017-05-23 17:22:46 +0800

    获取数据

--- src/app/Dateviewer/Dateviewer.js
+++ src/app/Dateviewer/Dateviewer.js
@@ -1,4 +1,12 @@
-import {Container, Icon, Table} from 'semantic-ui-react'
+import {
+  Container,
+  Dimmer,
+  Icon,
+  Loader,
+  Segment,
+  Select,
+  Table,
+} from 'semantic-ui-react'
 
 import React from 'react'
 import fetch from '../../Fetch'
@@ -21,18 +29,35 @@ const Row = (props) => {
   const data = props.data
   return (
     <Table.Row>
+      <Table.Cell>{data.id}</Table.Cell>
       <Table.Cell>{data.weibo_id}</Table.Cell>
-      <Table.Cell>{data.weibo_cont}</Table.Cell>
-      <Table.Cell negative>{data}</Table.Cell>
+      <Table.Cell positive>{data.weibo_cont}</Table.Cell>
+      <Table.Cell>{data.repost_num}</Table.Cell>
+      <Table.Cell>{data.comment_num}</Table.Cell>
+      <Table.Cell>{data.praise_num}</Table.Cell>
+      <Table.Cell>{data.uid}</Table.Cell>
+      <Table.Cell>{data.is_origin}</Table.Cell>
+      <Table.Cell>{data.weibo_url}</Table.Cell>
+      <Table.Cell>{data.create_time}</Table.Cell>
     </Table.Row>
   )
 }
 class Datatable extends React.Component {
-  componentDidMount() {}
+  componentDidMount = async() => {
+    this.setState({loading: 1})
+    const row = await fetch('http://localhost:3001/getWeiboData?pagenum=1&num=30', {method: 'get'})
+    console.log(row)
+    this.setState({data: row.rows, loading: 0})
+
+  }
+  state = {
+    loading: 1,
+    data: [],
+    count: 0,
+    num: 10
+  }
   render() {
-    const lists = [
-        1, 2, 3
-      ],
+    const lists = this.state.data,
       heads = [
         '序号',
         '微博id',
@@ -42,11 +67,29 @@ class Datatable extends React.Component {
         '点赞数',
         'uid',
         '是否原创',
-        '发表设备',
         '微博地址',
         '发表时间'
       ]
-      return (
+    let result = false,
+      loading = this.state.loading
+    if (loading) {
+      if (loading === 1) {
+        result = (
+          <Dimmer active inverted>
+            <Loader inverted>等待中</Loader>
+          </Dimmer>
+        )
+      } else {
+        result = null
+      }
+    }
+    const options=[
+      { key: 10, value: 10,  text: 10 },
+      { key: 20, value: 20,  text: 20 },
+      { key: 30, value: 30,  text: 30 }]
+    return (
+      <div>
+        <Select placeholder='选择每页页数' options={options}/>
         <Table celled>
           <Table.Header>
             <Table.Row>
@@ -60,6 +103,12 @@ class Datatable extends React.Component {
             {lists.map((list, i) => (<Row key={i} data={list}/>))}
           </Table.Body>
         </Table>
-      )
+        <Segment.Group horizontal>
+          <Segment>Left</Segment>
+          <Segment>Middle</Segment>
+          <Segment>Right</Segment>
+        </Segment.Group>
+      </div>
+    )
   }
 }
--- src/app/Separatewords/Separatewords.js
+++ src/app/Separatewords/Separatewords.js
@@ -1,4 +1,16 @@
-import {Button, Checkbox, Container, Dimmer, Divider, Header, Input, Loader, Message, Segment, Step} from 'semantic-ui-react'
+import {
+  Button,
+  Checkbox,
+  Container,
+  Dimmer,
+  Divider,
+  Header,
+  Loader,
+  Message,
+  Segment,
+  Step,
+  TextArea,
+} from 'semantic-ui-react'
 
 import React from 'react'
 import ajax from '../../Fetch'
@@ -8,65 +20,80 @@ class Separatewords extends React.Component {
     super()
     this.state = {
       value: '大佬真的好厉害',
-      loading:0
+      loading: 0
     }
   }
-  handleChange=event=> {
+  handleChange = event => {
     this.setState({value: event.target.value})
   }
-    focus = () => {
-    this.inputRef.focus()
+  focus = () => {
+    this
+      .inputRef
+      .focus()
   }
   Separatewords = async e => {
-    this.setState({loading:1})
+    this.setState({loading: 1})
     const res = await ajax('http://139.224.232.97:3001/fenci', {
       method: 'POST',
       body: {
         sentence: this.state.value
       }
     })
-   this.setState({loading:res.map(r=>{
-      let temp=''
-      for(let i in r){
-        temp+=i+':'+r[i]
-      }
-      return temp
-    })})
+    this.setState({
+      loading: res.map(r => {
+        let temp = ''
+        for (let i in r) {
+          temp += i + ':' + r[i]
+        }
+        return temp
+      })
+    })
   }
-    handleRef = c => {
+  handleRef = c => {
     this.inputRef = c
   }
   render() {
-    let result='',loading=this.state.loading
-if(loading)
-{
-  if(loading===1)
-  {
-    result=(      <Dimmer active inverted>
-        <Loader inverted>等待中</Loader>
-      </Dimmer>)
-  }else{
-    result=(  <Message>
-  <Segment.Group raised>{loading.map((result,i)=><Segment key={i}>{result}</Segment>)}
-  </Segment.Group></Message>      )
-  }
-}
+    let result = '',
+      loading = this.state.loading
+    if (loading) {
+      if (loading === 1) {
+        result = (
+          <Dimmer active inverted>
+            <Loader inverted>等待中</Loader>
+          </Dimmer>
+        )
+      } else {
+        result = (
+          <Message>
+            <Segment.Group raised>{loading.map((result, i) => (
+                <Segment key={i}>{result}</Segment>
+              ))}
+            </Segment.Group>
+          </Message>
+        )
+      }
+    }
     return (
 
       <Container text>
         <Header as='h2'>Header</Header>
 
         <Step.Group>
-           <Step>
+          <Step>
             <label>需要分割的词</label>
-             </Step>
-                <Step>
-            <Input ref={this.handleRef} onClick={this.focus} value={this.state.value} onChange={this.handleChange} placeholder='大佬真的好厉害'/>
-             </Step>
-            </Step.Group>
-          <div><Button onClick={this.Separatewords} >提交</Button></div>
-          {result}
-
+          </Step>
+          <Step>
+            <TextArea
+              autoHeight
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder='大佬真的好厉害'/>
+          </Step>
+        </Step.Group>
+        <div>
+          <Button onClick={this.Separatewords}>提交</Button>
+        </div>
+        {result}
       </Container>
     )
   }
